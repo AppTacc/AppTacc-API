94717cb5cfcd9f5ae5334924fb81998c580904aa
ramiro <ramirocardelli89@gmail.com>
2022-11-12 09:12:16 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import comerciosRouter from "./controllers/comercio";
 import categoriasRouter from "./controllers/categorias";
@@ -8,6 +9,7 @@ import dummyRouter from "./controllers/dummy";
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/comercios", comerciosRouter);
