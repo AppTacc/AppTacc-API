9153dd133807600906eb1973328796bb1e8e50f4
ramiro <ramirocardelli89@gmail.com>
2022-11-12 02:56:54 -0300

    Dummy

---
+++ src/controllers/dummy.ts
@@ -0,0 +1,40 @@
+import { CategoriaProducto, Comercio } from "@prisma/client";
+import { Router } from "express";
+import { CategoriaComercio } from "../types";
+import { getEnumValues } from "../util";
+
+const router = Router();
+
+router.get("/comercio", (req, res) => {
+    const comercio = {
+        nombre: "Sol de Invierno",
+        categorias: ["Para llevar"],
+        direccion: "25 de Mayo 3891",
+        latitud: -37.99046981482626,
+        longitud: -57.558342038624005,
+        horario: "9:30 - 20:30",
+        facebookURL: "https://www.facebook.com/soldeinviernosingluten/",
+        instagramURL: "",
+        twitterURL: "",
+        productos: [],
+        estrellas: 5,
+        ratingPrecios: 3,
+        productosDiabeticos: true,
+        productosVegetarianos: true,
+    };
+    res.status(200).json(comercio);
+});
+
+router.get("/categorias/comercio", (req, res) => {
+    res.status(200).json([
+        "Restaurante",
+        "Bar",
+        "Cafe",
+        "Takeaway"]);
+});
+
+router.get("/categorias/producto", (req, res) => {
+    res.status(200).json([...Object.values(getEnumValues(CategoriaProducto))]);
+});
+
+export default router;
