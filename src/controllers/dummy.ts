d31bce6fbe11680e9d5974d5a396ec2fc11bc2b1
ramiro <ramirocardelli89@gmail.com>
2022-11-12 03:16:09 -0300

    dummy comercios

--- src/controllers/dummy.ts
+++ src/controllers/dummy.ts
@@ -24,6 +24,31 @@ router.get("/comercio", (req, res) => {
     res.status(200).json(comercio);
 });
 
+router.get("/comercios", (req, res) => {
+    const comercio = {
+        nombre: "Sol de Invierno",
+        categorias: ["Para llevar"],
+        direccion: "25 de Mayo 3891",
+        latitud: -37.99046981482626,
+        longitud: -57.558342038624005,
+        horario: "9:30 - 20:30",
+        facebookURL: "https://www.facebook.com/soldeinviernosingluten/",
+        instagramURL: "",
+        twitterURL: "",
+        productos: [],
+        estrellas: 5,
+        ratingPrecios: 3,
+        productosDiabeticos: true,
+        productosVegetarianos: true,
+    };
+    const comercios = [
+        comercio,
+        comercio,
+        comercio
+    ]
+    res.status(200).json(comercios);
+})
+
 router.get("/categorias/comercio", (req, res) => {
     res.status(200).json([
         "Restaurante",
