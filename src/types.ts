a960de52976dac43da6a79ee5838e885b6d70b32
RenzoRomeo <renzoromeo44@gmail.com>
2023-01-29 20:20:15 -0300

    Categorias "Emprendimiento" y "Cocina Exclusiva"

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ export enum CategoriaComercio {
 	Restaurante = "Restaurante",
 	Bar = "Bar",
 	Cafe = "Cafe",
-	ParaLlevar = "Para Llevar"
+	ParaLlevar = "Para Llevar",
+	CocinaExclusiva = "Cocina Exclusiva",
+	Emprendimiento = "Emprendimiento"
 }
-
